1c70da80bae1bb251f0d0d2d1ae03e65b06f08d5
PCMS1 <PCMS1@DESKTOP-B78A9U2>
2026-02-24 11:38:22 +0530

    Add new case service ComplaintRegister

--- Src/Services/CaseService/Case.Repository/UnitOfwork/IUnitOfWorkRepository.cs
+++ Src/Services/CaseService/Case.Repository/UnitOfwork/IUnitOfWorkRepository.cs
@@ -1,10 +1,12 @@
 ﻿
 using Case.Repository.DierRegistrations;
+using Case.Repository.ComplaintRegister;
 
 namespace Case.Repository.UnitOfwork
 {
     public interface IUnitOfWorkRepository
     {
         IDierRegistrationsRepository DierRegistrations { get; }
+        IComplaintRegisterRepository ComplaintRegister { get; }
     }
 }
--- Src/Services/CaseService/Case.Repository/UnitOfwork/UnitOfWorkRepository.cs
+++ Src/Services/CaseService/Case.Repository/UnitOfwork/UnitOfWorkRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Case.Repository.DierRegistrations;
+using Case.Repository.ComplaintRegister;
 
 namespace Case.Repository.UnitOfwork
 {
-    public class UnitOfWorkRepository(IDierRegistrationsRepository DierRegistrations) : IUnitOfWorkRepository
+    public class UnitOfWorkRepository(IDierRegistrationsRepository DierRegistrations, IComplaintRegisterRepository ComplaintRegister ) : IUnitOfWorkRepository
     {
         public IDierRegistrationsRepository DierRegistrations { get; set; } = DierRegistrations;
+        public IComplaintRegisterRepository ComplaintRegister { get; set; } = ComplaintRegister;
     }
 }
